a9ffba087e5e6a60a9a286e78341cf444a60d5d3
Frank Brammer <fbrammer@brammeris.com>
2018-04-09 09:08:15 -0400

    Finished project (almost).

--- Emoji Dictionairy/BigViewController.swift
+++ Emoji Dictionairy/BigViewController.swift
@@ -9,13 +9,27 @@
 import UIKit
 
 class BigViewController: UIViewController {
-
+    
+    var emoji = "NO EMOJI"
+    @IBOutlet weak var emojiLabel: UILabel!
+    @IBOutlet weak var definitionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        emojiLabel.text = emoji
+        
+        if emoji == "😀" {
+            definitionLabel.text = "Smily Face"
+        } else if emoji == "😎" {
+            definitionLabel.text = "Sunglasses"
+        } else {
+            definitionLabel.text = "None"
+        }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
