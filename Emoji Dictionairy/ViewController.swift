49b5f5d7ec335709b53f5ce3000ff206777982ed
Frank Brammer <fbrammer@brammeris.com>
2018-04-08 21:27:09 -0400

    Added Modal Control, then Navigational Control, then switched to Show method.

--- Emoji Dictionairy/ViewController.swift
+++ Emoji Dictionairy/ViewController.swift
@@ -32,6 +32,11 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         cell.textLabel?.text = emojis[indexPath.row]
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "moveSegway", sender: "Any")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
